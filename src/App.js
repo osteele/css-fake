162c81103078a4b765717bddfa5a7912133d2393
Oliver Steele <steele@osteele.com>
2019-10-12 15:03:52 +0800

    Click to refresh an individual item

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ function App() {
     setSpinning(true);
     setTimeout(() => setSpinning(false), 1000);
   }
+  function refreshItem(i) {
+    const item = { name: makeNewName(), description: makeDescription() }
+    setEntries([...entries.slice(0, i), item, ...entries.slice(i + 1)])
+  }
   return (
     <div className="container">
       <h1>Rare CSS
@@ -22,8 +26,8 @@ function App() {
       <dl className="list-group">
         {windows(entries, 2).map((row, i) =>
           <div className="row" key={i}>
-            {row.map(({ name, description }, i) =>
-              <div key={i} className="col-sm list-group-item" >
+            {row.map(({ name, description }, j) =>
+              <div key={j} className="col-sm list-group-item" onClick={() => refreshItem(2 * i + j)}>
                 <dt>{name}</dt>
                 <dd>{description}</dd>
               </div>
