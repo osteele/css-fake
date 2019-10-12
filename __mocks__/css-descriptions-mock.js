7fff18bcfe2234f91a9908b08e6f1dd7b5fcbedb
Oliver Steele <steele@osteele.com>
2019-10-12 14:09:44 +0800

    Fix npm test

---
+++ __mocks__/css-descriptions-mock.js
@@ -0,0 +1,9 @@
+const text = `A description
+A longer description
+A very long description
+A very very long description
+A shorter description
+Another description
+And yet another description`;
+
+export default text
---
+++ __mocks__/css-names-mock.js
@@ -0,0 +1,6 @@
+const text = `attr1
+attr2
+start1-middle1-end1
+start2-middle2-end2`;
+
+export default text
