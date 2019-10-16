67e22fd98fd6c4fc75a5c95b6ad4b71f1b586cd0
Oliver Steele <steele@osteele.com>
2019-10-16 13:40:24 +0800

    Add examples for hooks

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import { default as React, useState } from 'react';
 import './App.css';
 import { fakeAttributeName } from './fakeCssAttributeNames';
 import { fakeDescription } from './fakeCssDescriptions';
-import { useMutexProtection, useReplaceableTimeout } from './hooks';
+import { useMutexGuard, useRestartableTimeoutEffect } from './hooks';
 import { arrayWindows, stringCompare } from './utils';
+
 
 function App() {
   const [entries, setEntries] = useState(fakeEntries());
-  const [scheduleReplaceAll, spinning] = useMutexProtection((release) => {
+  const [scheduleReplaceAll, spinning] = useMutexGuard((release) => {
     setTimeout(() => {
       replaceAllItems();
       release();
     }, 1000);
   });
 
-  useReplaceableTimeout(() => {
+  useRestartableTimeoutEffect(() => {
     const match = window.location.search.match(/[?&]refresh(?:=(\d+))?($|&)/);
     if (match) {
       const seconds = Number(match[1] || 10000);
@@ -22,14 +23,19 @@ function App() {
     }
   });
 
-  const replaceAllItems = () => setEntries(fakeEntries());
+  const replaceAllItems = () =>
+    setEntries(fakeEntries());
   const replaceItem = (i) =>
     setEntries([...entries.slice(0, i), fakeEntry(), ...entries.slice(i + 1)]);
 
   return (
     <div className="container">
       <h1>Rare CSS
-          <i title="Click to refresh" onClick={scheduleReplaceAll} className={`${spinning && 'fa-spin'} fa fa-refresh`} aria-hidden="true"></i>
+          <i
+          title="Click to refresh"
+          onClick={scheduleReplaceAll}
+          className={`fa fa-refresh ${spinning && 'fa-spin'}`}
+          aria-hidden="true" />
         <small className="text-muted">Lesser-Known CSS Attributes</small>
       </h1>
 
@@ -37,7 +43,10 @@ function App() {
         {arrayWindows(entries, 2).map((row, i) =>
           <div className="row" key={i}>
             {row.map(({ name, description }, j) =>
-              <div key={j} className="col-sm list-group-item" onClick={() => replaceItem(2 * i + j)}>
+              <div
+                key={j}
+                className="col-sm list-group-item"
+                onClick={() => replaceItem(2 * i + j)}>
                 <dt>{name}</dt>
                 <dd>{description}</dd>
               </div>
--- src/hooks.js
+++ src/hooks.js
@@ -3,14 +3,23 @@ import { useEffect, useRef, useState } from 'react';
 /**
  * Accepts a function that creates a timeout. If the function returns a
  * setTimeout id, the timeout is cancelled if the function is called again.
+ *
+ * ```
+ * // The old timer is cancelled and a new timer starts each time the
+ * // component is rendered.
+ * function Component() {
+ *   useRestartableTimeoutEffect(() =>
+ *     setTimeout(() => console.info('done'), 1000));
+ *   ...
+ * }
  */
-export function useReplaceableTimeout(callback) {
+export function useRestartableTimeoutEffect(effect) {
     const timeoutIdRef = useRef(null);
     useEffect(() => {
         if (timeoutIdRef.current) {
             clearTimeout(timeoutIdRef.current);
         }
-        const timeoutId = callback();
+        const timeoutId = effect();
         timeoutIdRef.current = timeoutId;
     });
 }
@@ -21,8 +30,21 @@ export function useReplaceableTimeout(callback) {
  *
  * Returns a function that guards the callback, and a value that indicates
  * whether the function is currently running.
+ *
+ * ```
+ * const [scheduleRefresh, isRefreshing] = useMutexGuard((done) => {
+ *   fetch('http://example.com/movies.json')
+ *     .then((response) => response.json())
+ *     .then(setData)
+ *     .finally(done);
+ * });
+ *
+ * <i
+ *   onClick={scheduleRefresh}
+ *   className={`fa fa-refresh ${isRefreshing && 'fa-spin}`}/>
+ * ```
 */
-export function useMutexProtection(callback) {
+export function useMutexGuard(callback) {
     const [isRunning, setIsRunning] = useState(false);
     function guardedCallback() {
         if (isRunning) { return; }
